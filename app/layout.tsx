d6b9a8f7413d8caf9938a5022d994099215851b7
Shristi Gupta <91966319+shristigupta12@users.noreply.github.com>
2024-02-07 15:28:12 +0530

    removed the html tag and used empty tag instead to fix deployment issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-"use client"
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -21,23 +20,35 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" >
+    <>
       <Head>
-          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-57T2QPNW');</script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-57T2QPNW');
+            `,
+          }}
+        />
       </Head>
-      <body className={cn("bg-neutral-50 dark:bg-neutral-900", inter.className )}>
-          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-57T2QPNW"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <body className={cn("bg-neutral-50 dark:bg-neutral-900", inter.className)}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-57T2QPNW"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <ThemeProvider attribute="class">
-          <Navbar/>
+          <Navbar />
           {children}
           <Toaster />  
         </ThemeProvider>
       </body>
-    </html>
+    </>
   )
 }
