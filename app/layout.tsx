7a7cf7cd949742dd98f830a88765862a2272bc00
Shristi Gupta <91966319+shristigupta12@users.noreply.github.com>
2024-02-07 13:27:19 +0530

    Test GTM configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,7 @@ import { cn } from "@/lib/utils"
 import { ThemeProvider } from "next-themes"
 import { Navbar } from '@/components/sections/navbar'
 import { Toaster } from '@/components/ui/toaster'
-
-
+import Head from "next/head";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,8 +21,21 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" >  
+    <html lang="en" >
+      <Head>
+        <!-- Google Tag Manager -->
+          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-57T2QPNW');</script>
+        <!-- End Google Tag Manager -->
+      </Head>
       <body className={cn("bg-neutral-50 dark:bg-neutral-900", inter.className )}>
+        <!-- Google Tag Manager (noscript) -->
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-57T2QPNW"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         <ThemeProvider attribute="class">
           <Navbar/>
           {children}
