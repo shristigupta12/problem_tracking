91b989c6a706575a76b8945821608957a9104a19
Shristi Gupta <91966319+shristigupta12@users.noreply.github.com>
2024-02-07 15:10:57 +0530

    comment bug fix try

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,19 +23,15 @@ export default function RootLayout({
   return (
     <html lang="en" >
       <Head>
-        <!-- Google Tag Manager -->
           <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-57T2QPNW');</script>
-        <!-- End Google Tag Manager -->
       </Head>
       <body className={cn("bg-neutral-50 dark:bg-neutral-900", inter.className )}>
-        <!-- Google Tag Manager (noscript) -->
           <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-57T2QPNW"
           height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
         <ThemeProvider attribute="class">
           <Navbar/>
           {children}
